caf995dec56d1dfce0afbda0695c65f3e5a83a7c
Katie Myers <kathryn.myers@ingrammicro.com>
2019-10-07 12:00:24 -0500

    Restructuring of project to actually work

---
+++ DMScreen/DMData/NPCData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows;
+
+namespace DMScreen.DMData
+{
+    public class NPCData
+    {
+        public string GetNPCBond()
+        {
+            string bond = "";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(AppConfig.DynamicsConnectionString))
+                {
+                    SqlCommand cmd = conn.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "SELECT Bond FROM NPCBonds WHERE ID=3";
+                    cmd.CommandTimeout = 0;
+
+                    //cmd.Parameters.AddWithValue("@Id", roll);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                bond = "Enjoys not having access to the database";
+            }
+
+            return bond;
+        }
+    }
+}
